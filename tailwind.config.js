9a8eb5587dbbaf3480933f104d415daed71248ce
gdion77-dev <gdion77@gmail.com>
2026-03-24 19:20:14 +0200

    initial upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'deep-navy': '#050b18',
+        'navy': '#0a1d37',
+        'teal': '#5ba4b1',
+      },
+      fontFamily: {
+        sans: ['Manrope', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        serif: ['Manrope', 'sans-serif'],
+        mono: ['JetBrains Mono', 'ui-monospace', 'SFMono-Regular', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
