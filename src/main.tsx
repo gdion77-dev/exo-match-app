b425b01edaac9564f41b7ed6fe5e0c27b493dcf1
gdion77-dev <gdion77@gmail.com>
2026-03-25 08:50:05 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
